78e69f60ecc061af7276688b797d7d3a6713afde
ilyaprog93 <ilyaprog93@mail.ru>
2023-08-19 20:51:16 +0300

    класс массива элементов Создание+ Вывод+ Сравнение + Копирование + Исправлена ошибка создания объекта по объекту

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,14 +13,20 @@ public class Main {
         System.out.println(ms);
     }
     public static void main(String[] args) {
-        Massiv<Integer> mas1 = new Massiv();
-        Massiv<Integer> mas2 = new Massiv(15);
+        Massiv mas1 = new Massiv();
+        Massiv mas2 = new Massiv(15);
         PrintMas("Массив 1:",mas1); PrintMas("Массив 2:",mas2);
 
         mas1.RestartParam();
         mas2.RestartParam();
 
         PrintMas("Массив 1 Инициализация параметров:",mas1); PrintMas("Массив 2 Инииализация параметров:",mas2);
-        ArrayList<Integer> mas;
+
+        Massiv mas3 = mas2.clone();
+        PrintMas("Массив 3 Инииализация параметров:",mas2);
+
+        System.out.println("Сравнение массива 1 и 2:"+mas1.equals(mas2));
+        System.out.println("Сравнение массива 1 и 3:"+mas1.equals(mas3));
+        System.out.println("Сравнение массива 3 и 2:"+mas3.equals(mas2));
     }
 }
--- src/main/java/org/example/Massiv.java
+++ src/main/java/org/example/Massiv.java
@@ -5,48 +5,73 @@ import java.util.stream.Stream;
 
 interface InfMassiv
 {
+    public Object setEl(int index);
+    public void getEl(int index,Object el);
+    public int getSize();
     public String toString();
     public void RestartParam();
-    public Object clone();
-    public boolean equals(Object obj);
+    public boolean equals(Massiv obj);
 }
 
 
-public class Massiv<T> implements InfMassiv {
+public class Massiv implements InfMassiv {
 
-    Object mas[];
-    int size=10;
-    public Massiv()
-    {
-        mas =  new Object[size];
-    }
-    public Massiv(int size)
-    {
-        this.size = size;
+    private  Object mas[];
+    private int size = 10;
+
+    public Massiv() {
         mas = new Object[size];
     }
 
-    @Override
-    public String toString()
+    public Massiv(int size) {
+        this.size = size;
+        mas = new Object[size];
+    }
+    public Massiv(Massiv masObj)
     {
+        this.size = masObj.getSize();
+        this.mas = new Object[this.size];
+        for(int i=0;i<masObj.getSize();i++) this.getEl(i,masObj.setEl(i));
+    }
+    @Override
+    public Object setEl(int index) {
+        return this.mas[index];
+    }
+
+    @Override
+    public void getEl(int index,Object el) {
+        this.mas[index] = el;
+    }
+
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
+    @Override
+    public String toString() {
         StringBuffer res = new StringBuffer();
-        for(Object a:mas) res.append(" "+a);
+        for (Object a : mas) res.append(" " + a);
         return res.toString();
     }
+
     @Override
     public void RestartParam() {
-     for(int i=0;i<size;i++)
-         mas[i]=(int)(Math.random()*100+1);
+        for (int i = 0; i < size; i++)
+            mas[i] = (int) (Math.random() * 100 + 1);
     }
 
     @Override
-    public Object clone() {
-        return null;
+    public Massiv clone() {
+        Massiv res = new Massiv(this);
+        return res;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        return false;
+    public boolean equals(Massiv obj) {
+        if(this.size!=obj.getSize()) return false;
+        for(int i=0;i<this.size;i++)if(this.mas[i]!=obj.setEl(i)) return false;
+        return true;
     }
 
 

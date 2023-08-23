8a9e10b0dfee1e2e264d693addcf90ff126dfc0b
ilyaprog93 <ilyaprog93@mail.ru>
2023-08-23 21:43:24 +0300

    Добавление шаблонов для методов посл.поиска и бинарного

--- src/main/java/org/example/Search.java
+++ src/main/java/org/example/Search.java
@@ -5,4 +5,26 @@ package org.example;
  * Методы:
  */
 public class Search {
+    /**
+     * Алгоритм последовательного поиска
+     * @param array - массив целых чисел
+     * @param el - искомый элемент
+     * @return - индекс искомого элемента
+     */
+    public int SequentialSearch(Massiv array, int el)
+    {
+        return array.getSize()+1;
+    }
+
+    /**
+     * Алгоритм логарифмического(бинарного) поиска
+     * @param array - массив целых чисел
+     * @param el - искомый элемент
+     * @return - индекс искомого элемента
+     */
+    public int BinarySearch(Massiv array, int el)
+    {
+        return array.getSize()+1;
+    }
+
 }
